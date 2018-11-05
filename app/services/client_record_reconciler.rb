d039e3623642909a6957eb5c6e1a3db2f1f1fe87
Anthony M <anthony@followapp.me>
2018-11-05 11:15:43 -0500

    Update client balance with new invoices and payments after reconciling duplicate records

--- app/services/client_record_reconciler.rb
+++ app/services/client_record_reconciler.rb
@@ -18,7 +18,14 @@ class ClientRecordReconciler
     end
 
     reconciled_addresses.update_all(billing_address: false)
+    Client.joins(:invoices, :payments).find_each do |client|
+      invoices_total = client.invoices.sum(:total)
+      payments_total = client.payments.sum(:amount)
+      client.update_column(:balance, (invoices_total + payments_total))
+    end
   end
+
+  private
 
   def relationships_to_update
     [:invoices, :payments, :estimates, :addresses]
